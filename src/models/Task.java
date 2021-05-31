50ea47ad6083fe1f6a1e442ba936fa4c9ade726c
oonisi-ikuto <ikuto@allexceed.co.jp>
2021-05-31 22:33:39 +0900

    Lesson6

--- src/models/Task.java
+++ src/models/Task.java
@@ -1,5 +1,4 @@
 package models;
-
 import java.sql.Timestamp;
 
 import javax.persistence.Column;
@@ -7,9 +6,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTask",
+        query = "SELECT t FROM Task AS t ORDER BY t.id DESC"
+            )
+        })
 @Table(name = "tasks")
 public class Task {
     @Id
